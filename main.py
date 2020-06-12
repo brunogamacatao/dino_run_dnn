75e97fb8f78c16f2511470bc01ae2103f0697c97
brunogamacatao@gmail.com <brunogamacatao@gmail.com>
2020-06-11 22:04:18 -0300

    Atualizei o codigo

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ import torch.optim as optim
 class ControlaDino(nn.Module):
   def __init__(self):
     super(ControlaDino, self).__init__()
-    self.camada1 = nn.Linear(3, 12)
+    self.camada1 = nn.Linear(2, 12)
     self.camada2 = nn.Linear(12, 6)
-    self.camada3 = nn.Linear(6, 1)
+    self.camada3 = nn.Linear(6, 2)
 
   def forward(self, x):
     x = F.relu(self.camada1(x))
@@ -65,33 +65,33 @@ def executa_jogo(rede):
   while True:
     screen = dino.grab_screen()
 
-    obstaculo0 = min(1, float(get_obstaculo(screen, -20)))
-    obstaculo1 = min(1, float(get_obstaculo(screen)))
-    obstaculo2 = min(1, float(get_obstaculo(screen, 20)))
-    saida_rede = rede(torch.tensor([obstaculo0, obstaculo1, obstaculo2]))
-    acao = saida_rede.item()
+    obstaculo = min(1, float(get_obstaculo(screen)))
+    acc = dino.get_acceleration()
+    saida_rede = rede(torch.tensor([obstaculo, acc]))
+    acao  = saida_rede.data[0]
+    delay = saida_rede.data[1]
 
-    print('ob0: {} - ob1: {} - ob2: {} - acao: {}'.format(obstaculo0, obstaculo1, obstaculo2, acao))
+    print('ob: {} - acc: {} - acao: {} - delay: {}'.format(obstaculo, acc, acao, delay))
 
     if acao > 0.5:
-      #time.sleep(0.3) # delay
+      time.sleep(0.3) # delay
       dino.press_up() # pula
    
     if dino.is_crashed():
       return dino.get_score(), entradas, saidas
     else:
       # adiciona apenas pares de entrada/saída de sucesso
-      entradas.append([obstaculo0, obstaculo1, obstaculo2])
+      entradas.append([obstaculo, acc])
       saidas.append(acao)
     
     time.sleep(0.1)
 
 # Dados de treinamento iniciais
-entradas = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0]]
-saidas   = [0, 0.1, 0.3, 1]
+entradas = [[0, 6], [1, 6]]
+saidas   = [[0, 0.3], [1, 0.3]]
 treina(entradas, saidas)
 
-melhor_score = 5
+melhor_score = 50
 
 while True:
   rede.eval() # desliga o modo treinamento
